599805859bd6358a28b8ffcacaeb1af53c5a98e2
billDrett <dendro12@live.com>
2020-09-22 21:43:14 +0200

    [WIP] Instragram bot to find all followers and followes and check how many don't match.

---
+++ scrappers/instragram_bot.py
@@ -0,0 +1,65 @@
+from selenium import webdriver
+import chromedriver_binary  # Adds chromedriver binary to path
+import time
+
+# TODO find a safe way to store the password and email
+# learn xpath to make the script search more resilient and redable
+USERNAME = ""
+PASSWORD = ""
+
+driver = webdriver.Chrome()
+driver.get("https://www.instagram.com/")
+time.sleep(3)
+driver.find_element_by_xpath(
+    '//*[@id="loginForm"]/div/div[1]/div/label/input').send_keys(USERNAME)
+driver.find_element_by_xpath(
+    '//*[@id="loginForm"]/div/div[2]/div/label/input').send_keys(PASSWORD)
+driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[3]/button').click()
+time.sleep(4)
+
+# ignore save screen
+driver.find_element_by_xpath(
+    '//*[@id="react-root"]/section/main/div/div/div/div/button').click()
+time.sleep(2)
+
+# ignore notification
+driver.find_element_by_xpath(
+    '/html/body/div[4]/div/div/div/div[3]/button[2]').click()
+time.sleep(2)
+
+# click on name
+driver.find_element_by_xpath(
+    '//*[@id="react-root"]/section/main/section/div[3]/div[1]/div/div[2]/div[1]/a').click()
+time.sleep(3)
+
+# following
+driver.find_element_by_xpath(
+    '//*[@id="react-root"]/section/main/div/header/section/ul/li[3]/a').click()
+
+time.sleep(2)
+
+# execute javascript to scroll
+suggestion = driver.find_element_by_xpath(
+    '/html/body/div[4]/div/div/div[2]/div[1]')
+
+driver.execute_script("arguments[0].scrollIntoView();", suggestion)
+time.sleep(2)
+
+# scroll to the buttom of the list
+scroll_box = driver.find_element_by_xpath(
+    '/html/body/div[4]/div/div/div[2]')
+
+last_height, height = 0, 1
+while last_height != height:
+    last_height = height
+    time.sleep(1)
+    height = driver.execute_script('''arguments[0].scrollTo(0, arguments[0].scrollHeight);
+                             return arguments[0].scrollHeight;''', scroll_box)
+    print("height is {}".format(height))
+
+# get all relevant names
+all_name_links = scroll_box.find_elements_by_tag_name('a')
+names = [link.text for link in all_name_links if link.text != '']
+print(names)
+
+# use this names for something usefull, for example to compare with the followers
